ec2491bc8a5537d4440c632c2cd6201d1b4314c5
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-06-27 08:04:35 +0100

    wip

--- src/composables/selectable.ts
+++ src/composables/selectable.ts
@@ -1,7 +1,13 @@
 import { ref, type ComputedRef, watch, type Ref } from 'vue';
+import { useKeyModifier } from '@vueuse/core';
 
 export const useSelectable = <T>(keyBy: keyof T, rows: ComputedRef<T[] | undefined>) => {
   const selected: Ref<T[]> = ref([]);
+
+  const meta = useKeyModifier('Meta');
+  const shift = useKeyModifier('Meta');
+
+  const rangeSelectionIndex = ref<number>();
 
   const comparator = (a: T) => (b: T) => a[keyBy] === b[keyBy];
 
@@ -12,6 +18,19 @@ export const useSelectable = <T>(keyBy: keyof T, rows: ComputedRef<T[] | undefin
   };
 
   const onRowClick = (row: T) => {
+    // console.log(meta.value);
+    // if (!rangeSelectionIndex.value) {
+    //   rangeSelectionIndex.value = rows.value.indexOf(row);
+    //   return;
+    // }
+    // const other = rows.value.indexOf(row);
+    // const l = Math.min(rangeSelectionIndex.value, other);
+    // const r = Math.max(rangeSelectionIndex.value, other);
+    // for (let i = l; i <= r; i++) {
+    //   selected.value = [...selected.value, rows.value[i]];
+    // }
+    // rangeSelectionIndex.value = undefined;
+
     if (isSelected(row)) {
       selected.value.splice(selected.value.findIndex(comparator(row)), 1);
     } else {
